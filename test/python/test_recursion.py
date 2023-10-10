bd466313c0218ea4110c9b1bf07bdea78b5256e8
HiPhish <hiphish@posteo.de>
2023-10-10 21:13:05 +0200

    Move Python test files around
    Move files to more idiomatic positions.

--- test/python/test_recursion.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from .rectoys.add import naive_add
-
-
-@pytest.mark.parametrize('x,y', [(2, 3), (0, 4), (7, 0)])
-def test_test_naive_add(x: int, y: int):
-    assert x + y == naive_add(x, y)
---
+++ test/python/tests/test_recursion.py
@@ -0,0 +1,12 @@
+import pytest
+from rectoys.add import naive_add, loop_add
+
+
+@pytest.mark.parametrize('x,y', [(2, 3), (0, 4), (7, 0)])
+def test_naive_add(x: int, y: int):
+    assert x + y == naive_add(x, y)
+
+
+@pytest.mark.parametrize('x,y', [(2, 3), (0, 4), (7, 0)])
+def test_naive_add(x: int, y: int):
+    assert x + y == loop_add(x, y)
